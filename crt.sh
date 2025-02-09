b1d768aa8cf20c4e648ca36dfbbee4953c586db5
Uriel-SG <65650311+Uriel-SG@users.noreply.github.com>
2025-02-09 22:24:21 +0100

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -3,17 +3,18 @@
 if [ -z "$1" ]; then
     echo "Please specify a target as argument"
     
-    echo "Usage: ./crt.sh <target>"
+    echo "Usage: ./crtsh.sh <target>"
     exit 1
 fi
 
 if [ "$1" == "-h" ]; then
-    echo "Usage: ./crt.sh <target>"
+    echo "Usage: ./crtsh.sh <target>"
     exit 0
 fi
 
 curl --output $1.json "https://crt.sh/?q=%25.$1&output=json"
 cat "$1.json" | jq .
 echo "--------------------------------------"
-echo -e "\nSubdomains found:\n"
-cat "$1.json" | jq -r --arg target "$1" '.[].common_name | select(test($target))' | sort -u
+echo "\nSubdomains trovati:\n"
+cat "$1.json" | jq -r --arg target "$1" '.[].name_value' | grep "$1" | tr '\n' '\n' | sort -u
+rm -f "$1.json"
